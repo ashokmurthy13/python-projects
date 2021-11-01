954085080247dffe8b89874677af96cd010da224
Ashok Murthy <ashokmurthy88@gmail.com>
2021-11-01 23:35:23 +0900

    find and replace text from file

--- files/file_counts.py
+++ files/file_counts.py
@@ -30,5 +30,15 @@ def statistics_opt(file_name):
             "characters": sum(len(line) for line in lines)}
 
 
+def find_and_replace(file_name, search_for, replace_with):
+    with open(file_name, "r+", encoding="UTF-8") as file:
+        data = file.read()
+        new_data = data.replace(search_for, replace_with)
+        file.seek(0)
+        file.write(new_data)
+        file.truncate()
+
+
 print(statistics("story.txt"))
 print(statistics_opt("story.txt"))
+find_and_replace("story.txt", "Alice", "Ashok")
